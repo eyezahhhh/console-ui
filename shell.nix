2eecb9511d4d518c6bad59eec0d2427a46c81ae7
eyezah (Gigabox) <git@eyezah.com>
2025-08-18 00:10:09 +1200

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nodejs
+    pkgs.electron
+    pkgs.glib
+    pkgs.gtk3
+    pkgs.dbus
+    pkgs.xorg.libX11
+    pkgs.libxkbcommon
+  ];
+
+  shellHook = ''
+    # Optional: fix locale warnings
+    export LC_ALL=en_US.UTF-8
+    export LANG=en_US.UTF-8
+
+    echo "Electron dev environment ready. Run 'npm start' or 'npx electron .'"
+  '';
+}
