1eb7ea1b62d2a0470941dc6eb075c09a78051824
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2025-08-18 00:10:09 +1200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+	root: path.resolve(__dirname, "src/renderer"),
+	base: "./",
+	server: {
+		port: 5173,
+	},
+	build: {
+		outDir: path.resolve(__dirname, "transpile/renderer"),
+		emptyOutDir: true,
+		rollupOptions: {
+			input: path.resolve(__dirname, "src/renderer/index.html"),
+		},
+	},
+	plugins: [react()],
+});
