7a1019ce1a37fc24907df4a1838fe41e42441aed
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2025-08-18 17:16:29 +1200

    push to work via teenybook

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,18 @@
 import { createRoot } from "react-dom/client";
+import "./global.scss";
+import styles from "./index.module.scss";
+import Machine from "@component/machine";
 
 function App() {
 	return (
-		<div>
+		<div className={styles.container}>
 			<h1>Hello!!!</h1>
+			<Machine
+				machine={{
+					address: "192.168.68.145",
+					name: "AuraSide VM",
+				}}
+			/>
 		</div>
 	);
 }
