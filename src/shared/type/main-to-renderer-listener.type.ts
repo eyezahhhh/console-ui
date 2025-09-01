73adf59cdc2181f09ad625f1eb0e8cc5203dfd8f
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2025-09-01 17:37:16 +1200

    add auto update

--- src/shared/type/main-to-renderer-listener.type.ts
+++ src/shared/type/main-to-renderer-listener.type.ts
@@ -1,9 +1,14 @@
 import ISettings from "@interface/settings.interface";
 import IMachine from "../interface/machine.interface";
+import IAvailableUpdate from "@interface/available-update.interface";
+import { ProgressInfo } from "electron-updater";
 
 type MainToRendererListener = {
 	machines: [IMachine[]];
 	pairing_code: [string, IMachine];
 	settings: [ISettings];
+	available_update: [IAvailableUpdate | null];
+	is_update_checking: [boolean];
+	update_status: [boolean, ProgressInfo | null];
 };
 export default MainToRendererListener;
