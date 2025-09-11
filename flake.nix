94c21891868ad8af7f692ac5556ba7a3a4343d92
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2025-09-11 09:52:06 +0000

    flake: Update to version 0.0.18

--- flake.nix
+++ flake.nix
@@ -10,16 +10,16 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        version = "0.0.17"; # FLAKE_UPDATE_MARKER_VERSION
+        version = "0.0.18"; # FLAKE_UPDATE_MARKER_VERSION
 
         sources = {
           "x86_64-linux" = {
             url = "[https://github.com/eyezahhhh/console-ui/releases/download/v$](https://github.com/eyezahhhh/console-ui/releases/download/v$){version}/Console-UI-${version}.AppImage";
-            sha256 = "1532ypnx27p7iy5sj69d060bz8i9743yw7fx5xpijvnsdhy8xsn3"; # FLAKE_UPDATE_MARKER_SHA256_X86_64
+            sha256 = "1vxm6gdb2zf01mql7b61gyjwg5dah9ii6h0i63y5bhi6wj2fgh9v"; # FLAKE_UPDATE_MARKER_SHA256_X86_64
           };
           "aarch64-linux" = {
             url = "[https://github.com/eyezahhhh/console-ui/releases/download/v$](https://github.com/eyezahhhh/console-ui/releases/download/v$){version}/Console-UI-${version}-arm64.AppImage";
-            sha256 = "0q55r7zzg7l45w1685khvadndnsqpmdf97jf5ggvlsvrzy0fsi38"; # FLAKE_UPDATE_MARKER_SHA256_AARCH64
+            sha256 = "15qv2gq8jzy3jbqyli063n690hrzdwlgxjabapicsh7r9kax0i8z"; # FLAKE_UPDATE_MARKER_SHA256_AARCH64
           };
         };
 
